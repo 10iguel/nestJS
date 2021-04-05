f31412306121d9e69de48d6073e30c0e2ef41768
Miguel <miguel.espinoza.silloca@gmail.com>
2021-04-05 13:42:06 -0500

    # Feature: Starting new project with nestJS - nest g module tasks - nest g controller tasks it is added automatically

---
+++ src/tasks/tasks.controller.ts
@@ -0,0 +1,6 @@
+import { Controller } from '@nestjs/common';
+
+/** nest command makes a lot of good stuff , g to generate and then you can generate module, controllers, providers and services */
+@Controller('tasks')
+export class TasksController {
+}
