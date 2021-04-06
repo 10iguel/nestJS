3389f1bf73f9c418515247e4f2106b1a6b654d7b
Miguel <miguel.espinoza.silloca@gmail.com>
2021-04-06 09:50:13 -0500

    # Feature: Update status using @param and @ body establishing status as the argument in the url.

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,6 +1,6 @@
-import { Body, Controller, Delete, Get, Param, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Post } from '@nestjs/common';
 import { TasksService } from './tasks.service';
-import { Task } from './task.model';
+import { Task, TaskStatus } from './task.model';
 import { CreateTaskDto } from './dto/create-task.dto';
 
 /** nest command makes a lot of good stuff , g to generate and then you can generate module, controllers, providers and services */
@@ -23,6 +23,14 @@ export class TasksController {
   @Post()
   createTask(@Body() createTaskDto: CreateTaskDto): Task {
     return this.tasksService.createTask(createTaskDto);
+  }
+
+  @Patch(':id/status')
+  updateStatusTask(
+    @Param('id') id: string,
+    @Body('status') status: TaskStatus,
+  ): Task {
+    return this.tasksService.updateTaskStatus(id, status);
   }
 
   @Delete('/:id')
