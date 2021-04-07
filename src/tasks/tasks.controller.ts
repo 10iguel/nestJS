16a93f4987170072988ff7422e30b56588ea7b19
Miguel <miguel.espinoza.silloca@gmail.com>
2021-04-06 21:15:41 -0500

    # Feature: - Custom Pipe Validating the tasks status - Validating tasks filtering and search

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -3,6 +3,7 @@ import { TasksService } from './tasks.service';
 import { Task, TaskStatus } from './task.model';
 import { CreateTaskDto } from './dto/create-task.dto';
 import { GetTasksFilterDto } from './dto/get-tasks-filter.dto';
+import { TaskStatusValidationPipe } from './pipes/task-status-validation.pipe';
 
 /** nest command makes a lot of good stuff , g to generate and then you can generate module, controllers, providers and services */
 /** This page is to have controllers and POST like createTask(@Body() and then pass arguments ) */
@@ -12,7 +13,7 @@ export class TasksController {
   }
 
   @Get()
-  getTasks(@Query() filterDto: GetTasksFilterDto): Task[] {
+  getTasks(@Query(ValidationPipe) filterDto: GetTasksFilterDto): Task[] {
     if (Object.keys(filterDto)) {
       return this.tasksService.getTaskWithFilters(filterDto);
     } else {
@@ -34,7 +35,7 @@ export class TasksController {
   @Patch(':id/status')
   updateStatusTask(
     @Param('id') id: string,
-    @Body('status') status: TaskStatus,
+    @Body('status', TaskStatusValidationPipe) status: TaskStatus,
   ): Task {
     return this.tasksService.updateTaskStatus(id, status);
   }
