bfc2c06c472da13216d6e7b810b3b7c2ccf3633c
Miguel <miguel.espinoza.silloca@gmail.com>
2021-04-19 22:05:08 -0500

    Some changes to connect to the frontend

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -8,6 +8,7 @@ import { PassportModule } from '@nestjs/passport';
 import { JwtStrategy } from './jwt.strategy';
 import * as config from 'config';
 
+
 const jwtConfig = config.get('jwt');
 
 @Module({
--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,13 @@ import { AppModule } from './app.module';
 import { Logger } from '@nestjs/common';
 import * as config from 'config';
 
+//Main theme
 async function bootstrap() {
   const logger = new Logger('bootstrap');
   const app = await NestFactory.create(AppModule);
+  if (process.env.NODE_ENV === 'development') {
+    app.enableCors();
+  }
   const configServer = config.get('server');
   const port = process.env.PORT || configServer.port;
   await app.listen(port);
